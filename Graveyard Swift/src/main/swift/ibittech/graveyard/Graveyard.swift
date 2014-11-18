5a7dff8da253534783077f5eacd2c2180c774fe7
xbony2 <xbony22@gmail.com>
2014-11-18 17:24:18 -0500

    Made things nicer I guess

--- Graveyard Swift/src/main/swift/ibittech/graveyard/Graveyard.swift
+++ Graveyard Swift/src/main/swift/ibittech/graveyard/Graveyard.swift
@@ -1,13 +1,9 @@
 //
-//  Graveyard_Swift.h
+//  Graveyard.swift
 //  Graveyard Swift
 //
 //  Created by xbony2 on 11/18/14.
 //  Copyright (c) 2014 ibit-tech. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
-
-@interface Graveyard_Swift : NSObject
-
-@end
+import Foundation
