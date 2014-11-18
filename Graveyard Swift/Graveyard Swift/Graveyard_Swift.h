d01ad72026884516e6852ba53cc5d2229f16ab6e
xbony2 <xbony22@gmail.com>
2014-11-18 17:13:50 -0500

    Auto-generated files put in.

---
+++ Graveyard Swift/Graveyard Swift/Graveyard_Swift.h
@@ -0,0 +1,13 @@
+//
+//  Graveyard_Swift.h
+//  Graveyard Swift
+//
+//  Created by xbony2 on 11/18/14.
+//  Copyright (c) 2014 ibit-tech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Graveyard_Swift : NSObject
+
+@end
---
+++ Graveyard Swift/Graveyard Swift/Graveyard_Swift.m
@@ -0,0 +1,13 @@
+//
+//  Graveyard_Swift.m
+//  Graveyard Swift
+//
+//  Created by xbony2 on 11/18/14.
+//  Copyright (c) 2014 ibit-tech. All rights reserved.
+//
+
+#import "Graveyard_Swift.h"
+
+@implementation Graveyard_Swift
+
+@end
