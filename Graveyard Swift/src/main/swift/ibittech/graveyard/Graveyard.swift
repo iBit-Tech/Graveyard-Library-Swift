d1c82cf1eba289097c023b4744f6c2c37d7fd787
xbony2 <xbony22@gmail.com>
2014-11-18 20:19:28 -0500

    I hope that works?

--- Graveyard Swift/src/main/swift/ibittech/graveyard/Graveyard.swift
+++ Graveyard Swift/src/main/swift/ibittech/graveyard/Graveyard.swift
@@ -6,6 +6,26 @@
 //  Copyright (c) 2014 ibit-tech. All rights reserved.
 //
 
-import Foundation
-
+//Graveyard's name
 let name = "Graveyard"
+
+//Graveyard's extended name/description
+let fancyName = "Graveyard, the multipurpose swift library"
+
+/*
+* Graveyard's current version.
+*
+* Version system:
+* A change from 1.0.0a to 1.0.0b means I've fixed a small bug that I've probably just
+* found a minute after a release, most often just a one-liner.
+* A change from 1.0.0b to 1.0.1a means I've fixed a moderate bug or added/changed a
+* small feature.
+* A change from 1.0.1a to 1.0.6a means I've fixed an annoying bug or added/changed a
+* feature or two (or a few).
+* A change from 1.0.6a to 1.1.0a means I've added dozens of new features or changed
+* dozens of features slightly.
+* A change from 1.1.0a to 2.0.0a means I've added tons of stuff, removed tons of stuff
+* and basically rewritten large parts of the code. After version 1.9.Xx, 2.0.0a will
+* be next. Note you may have to rewrite parts of your code if you rely on this library.
+*/
+let version = "v1.0.0a"
